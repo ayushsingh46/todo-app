7118adf5cbada05f3c96fafa2e9ed64601ddb00f
ayushsingh46 <ayushsingh46@gmail.com>
2026-05-14 11:40:58 +0530

    first commit

---
+++ todo/todo/views.py
@@ -0,0 +1,79 @@
+from django . shortcuts import render, redirect
+from django.contrib.auth.models import User
+from todo import models
+from todo.models import TODOO
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+def signup(request):
+    if request.method=='POST':
+      fnm=request.POST.get('fnm')  
+      emailid=request.POST.get('emailid') 
+      pwd=request.POST.get('pwd') 
+      print(fnm,emailid,pwd) 
+      my_user=User.objects.create_user(fnm,emailid,pwd)
+      my_user.save()
+      return redirect('login')
+     
+     
+     
+     
+    return render(request, 'signup.html')
+  
+def login_view(request):
+  if request.method=="POST":
+    fnm=request.POST.get('fnm')  
+    pwd=request.POST.get('pwd')
+    print(fnm,pwd)  
+    user=authenticate(request,username=fnm,password=pwd)
+    if user is not None:
+      login(request,user)
+      return redirect('/todopage')
+    else:
+      messages.info(request,'credentials invalid')
+      return redirect('/login')
+    
+    
+  return render(request,'login.html')  
+
+def todo(request):
+  if request.method=="POST":
+    title=request.POST.get('title')
+    print(title)
+    obj=models.TODOO(title=title,user=request.user)
+    obj.save()
+    res=models.TODOO.objects.filter(user=request.user).order_by('-date')
+    return redirect('/todopage',{'res':res})
+  res=models.TODOO.objects.filter(user=request.user).order_by('-date')
+  return render(request,'todo.html',{'res':res})  
+
+def delete_todo(request,srno):
+    print(srno)
+    obj=models.TODOO.objects.get(srno=srno)
+    obj.delete()
+    return redirect('/todopage')
+  
+@login_required(login_url='/login')
+
+
+def edit_todo(request,srno):
+  if request.method=="POST":
+    title=request.POST.get('title')
+    print(title)
+    obj=models.TODOO.objects.get(srno=srno)
+    obj.title=title
+    obj.save()
+    user=request.user
+    res=models.TODOO.objects.filter(user=request.user).order_by('-date')
+    return redirect('/todopage',{'res':res})
+  
+  obj=models.TODOO.objects.get(srno=srno)
+  res=models.TODOO.objects.filter(user=request.user).order_by('-date')
+  return render(request,'todo.html',{'res':res})  
+
+
+def signout(request):
+    logout(request)
+    return redirect('/login')
